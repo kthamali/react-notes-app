3dfd13ea84c35a4d8c77643e8d273977a4ba9b29
Thamali Fonseka <50350183+kthamali@users.noreply.github.com>
2023-11-07 22:16:23 -0500

    Fewer updates
    Added NotesList, Note & AddNote components.

---
+++ src/components/Note.js
@@ -0,0 +1,14 @@
+const Note = () => {
+    return (
+        <div className="note">
+            <span>First Note!!</span>
+            <div className="note-footer">
+            </div>
+
+        </div>
+
+    );
+
+};
+
+export default Note;
---
+++ src/components/NotesList.js
@@ -0,0 +1,10 @@
+const NotesList = () => {
+    return (
+        <div className="notes-list">
+        <Note />
+        </div>
+    );
+
+};
+
+export default NotesList;
